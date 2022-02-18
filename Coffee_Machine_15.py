eda3a356beaaf66fb02db738ed83575f2c3b92d2
Matt Santos <mattsantos541@gmail.com>
2022-02-18 15:02:22 -0600

    finished coffee project

--- Coffee_Machine_15.py
+++ Coffee_Machine_15.py
@@ -1,4 +1,5 @@
-from coffee_menu_15 import MENU, resources, print_report, is_resources_sufficient
+from coffee_menu_15 import MENU, resources, print_report, is_resources_sufficient, \
+    process_coins, is_transaction_successful, make_coffee
 
 # TODO Prompt user by asking What would you like? (espresso/latte/cappuccino):
 profit = 0
@@ -6,7 +7,7 @@ profit = 0
 is_on = True
 
 while is_on:
-    choice = input("What wpould you like?  (espresso/latte/cappuccino): ")
+    choice = input("What would you like?  (espresso/latte/cappuccino): ")
     if choice == "off":
         is_on = False
 # TODO Print report. a. When the user enters “report” to the prompt, a report should be generated that shows the current resource values.
@@ -16,3 +17,7 @@ while is_on:
         drink = MENU[choice]
         print(drink)
         is_resources_sufficient(drink["ingredients"])
+        payment = process_coins()
+        if is_transaction_successful(payment, drink["cost"]):
+            make_coffee(choice, drink["ingredients"])
+
--- coffee_menu_15.py
+++ coffee_menu_15.py
@@ -29,7 +29,7 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
-
+profit = 0
 def print_report():
     print(f'Water: {resources["water"]}ml')
     print(f'Milk: {resources["milk"]}ml')
@@ -44,3 +44,31 @@ def is_resources_sufficient(order_ingredients):
             return False
         else:
             return True
+def process_coins():
+    print("Please insert coins")
+    total = int(input("how many quarters?: ")) * .25
+    total += int(input("how many dimes?: ")) * .10
+    total += int(input("how many nickels?: ")) * .05
+    return total
+
+def is_transaction_successful(money_recieved, drink_cost):
+    """return true when payment successful, return false if money is insufficient"""
+    if money_recieved >= drink_cost:
+        change = round(money_recieved - drink_cost, 2)
+        print(f"here is your change of {change}.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("That is not enough money.")
+        return False
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+        print("Here is your drink!")
+
+
+
+
+
+
